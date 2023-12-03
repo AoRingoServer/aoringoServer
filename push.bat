4b32a868813f4466df1c80db3b5a76c0a030625e
りんご飴196 <kazumac41@gmail.com>
2023-12-04 01:18:29 +0900

    Click Push 2023-12-04 at 01:18

--- push.bat
+++ push.bat
@@ -1,5 +1,19 @@
 @echo off
 
+REM 現在の日時を取得
+for /f "delims=" %%a in ('wmic OS Get localdatetime ^| find "."') do set datetime=%%a
+
+REM 年月日と時刻のフォーマットを定義
+set "year=%datetime:~0,4%"
+set "month=%datetime:~4,2%"
+set "day=%datetime:~6,2%"
+set "hour=%datetime:~8,2%"
+set "minute=%datetime:~10,2%"
+
+REM コミットメッセージを生成
+set "commit_message=Click Push %year%-%month%-%day% at %hour%:%minute%"
+
+REM git add, commit, push を実行
 git add .
-git commit -m "click push"
+git commit -m "%commit_message%"
 git push
