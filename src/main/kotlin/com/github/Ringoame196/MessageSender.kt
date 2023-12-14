a263544797a8a1deaa7c5dad23f797c2b8a69749
りんご飴196 <kazumac41@gmail.com>
2023-12-14 22:37:57 +0900

    [削除]MessageSenderクラス削除

--- src/main/kotlin/com/github/Ringoame196/MessageSender.kt
+++
@@ -1,8 +0,0 @@
-package com.github.Ringoame196
-
-interface MessageSender {
-    fun createBossbar()
-    fun sendMessage(message: String)
-    fun sendErrorMessage(message: String)
-    fun sendActionBar(title: String)
-}
