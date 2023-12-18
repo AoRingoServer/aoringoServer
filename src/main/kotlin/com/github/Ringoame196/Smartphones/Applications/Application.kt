cc948c1dc04827ed1779e25a7460cdb2d725d15c
りんご飴196 <kazumac41@gmail.com>
2023-12-18 20:28:41 +0900

    ApplicationインターフェースにcloseGUIを追加

--- src/main/kotlin/com/github/Ringoame196/Smartphones/Applications/Application.kt
+++ src/main/kotlin/com/github/Ringoame196/Smartphones/Applications/Application.kt
@@ -1,9 +1,11 @@
 package com.github.Ringoame196.Smartphones.Applications
 
 import org.bukkit.entity.Player
+import org.bukkit.inventory.Inventory
 import org.bukkit.plugin.Plugin
 
 interface Application {
     fun getCustomModelData(): Int
     fun openGUI(player: Player, plugin: Plugin)
+    fun closeGUI(player: Player,gui:Inventory)
 }
