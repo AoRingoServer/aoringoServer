a5ea15c3d8b777a40081af2eeb9f88c66f55eab2
りんご飴196 <kazumac41@gmail.com>
2023-12-21 21:25:57 +0900

    インスタンス生成されないように

--- src/main/kotlin/com/github/Ringoame196/Smartphones/Applications/ConversionMoneyApplication.kt
+++ src/main/kotlin/com/github/Ringoame196/Smartphones/Applications/ConversionMoneyApplication.kt
@@ -12,11 +12,12 @@ class ConversionMoneyApplication : Application {
         return 2
     }
     override fun openGUI(player: Player, plugin: Plugin) {
+        val itemManager = ItemManager()
         val gui = Bukkit.createInventory(null, 9, "${ChatColor.BLUE}スマートフォン")
-        gui.setItem(1, ItemManager().make(Material.EMERALD, "${ChatColor.GREEN}100円", customModelData = 1))
-        gui.setItem(3, ItemManager().make(Material.EMERALD, "${ChatColor.GREEN}1000円", customModelData = 2))
-        gui.setItem(5, ItemManager().make(Material.EMERALD, "${ChatColor.GREEN}10000円", customModelData = 3))
-        gui.setItem(7, ItemManager().make(Material.EMERALD, "${ChatColor.GREEN}100000円", customModelData = 4))
+        gui.setItem(1, itemManager.make(Material.EMERALD, "${ChatColor.GREEN}100円", customModelData = 1))
+        gui.setItem(3, itemManager.make(Material.EMERALD, "${ChatColor.GREEN}1000円", customModelData = 2))
+        gui.setItem(5, itemManager.make(Material.EMERALD, "${ChatColor.GREEN}10000円", customModelData = 3))
+        gui.setItem(7, itemManager.make(Material.EMERALD, "${ChatColor.GREEN}100000円", customModelData = 4))
         player.openInventory(gui)
     }
 }
