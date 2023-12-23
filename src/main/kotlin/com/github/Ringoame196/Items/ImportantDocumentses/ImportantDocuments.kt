9208423937c592d3c3329e3902a389b69a01c5a9
りんご飴196 <kazumac41@gmail.com>
2023-12-24 01:56:21 +0900

    重要書類 マネージャー追加

--- src/main/kotlin/com/github/Ringoame196/Items/ImportantDocumentses/ImportantDocuments.kt
+++ src/main/kotlin/com/github/Ringoame196/Items/ImportantDocumentses/ImportantDocuments.kt
@@ -3,5 +3,6 @@ package com.github.Ringoame196.Items.ImportantDocumentses
 import org.bukkit.entity.Player
 
 interface ImportantDocuments {
+    val status:MutableList<String>
     fun displayCommand(player: Player)
 }
---
+++ src/main/kotlin/com/github/Ringoame196/Items/ImportantDocumentses/ImportantDocumentsManager.kt
@@ -0,0 +1,9 @@
+package com.github.Ringoame196.Items.ImportantDocumentses
+
+import com.github.Ringoame196.Items.WrittenBook
+
+class ImportantDocumentsManager() {
+    fun changeStat(status:MutableList<String>){
+
+    }
+}
