5fb69fd303b1db666d826b456fe6e8ac2af28da7
りんご飴196 <kazumac41@gmail.com>
2023-12-26 22:02:28 +0900

    インスタンスを生成しないように

--- src/main/kotlin/com/github/Ringoame196/Smartphones/Applications/TeleportApplication.kt
+++ src/main/kotlin/com/github/Ringoame196/Smartphones/Applications/TeleportApplication.kt
@@ -12,11 +12,12 @@ class TeleportApplication : Application {
         return 4
     }
     override fun openGUI(player: Player, plugin: Plugin) {
+        val itemManager = ItemManager()
         val gui = Bukkit.createInventory(null, 9, "${ChatColor.BLUE}スマートフォン")
-        gui.setItem(1, ItemManager().make(Material.CHEST, "${ChatColor.GOLD}ロビー"))
-        gui.setItem(3, ItemManager().make(Material.GRASS_BLOCK, "${ChatColor.GREEN}生活ワールド"))
-        gui.setItem(5, ItemManager().make(Material.DIAMOND_PICKAXE, "${ChatColor.AQUA}資源ワールド"))
-        gui.setItem(7, ItemManager().make(Material.QUARTZ_BLOCK, "${ChatColor.YELLOW}ショップ"))
+        gui.setItem(1, itemManager.make(Material.CHEST, "${ChatColor.GOLD}ロビー"))
+        gui.setItem(3, itemManager.make(Material.GRASS_BLOCK, "${ChatColor.GREEN}生活ワールド"))
+        gui.setItem(5, itemManager.make(Material.DIAMOND_PICKAXE, "${ChatColor.AQUA}資源ワールド"))
+        gui.setItem(7, itemManager.make(Material.QUARTZ_BLOCK, "${ChatColor.YELLOW}ショップ"))
         player.openInventory(gui)
     }
 }
