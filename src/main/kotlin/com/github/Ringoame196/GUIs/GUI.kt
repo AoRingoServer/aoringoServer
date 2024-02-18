ad631bdb5cb0fff2d8d965b7bbfaa386e624d92a
りんご飴196 <kazumac41@gmail.com>
2024-02-18 23:32:52 +0900

    playerData テンプレート 用意

--- src/main/kotlin/com/github/Ringoame196/GUIs/GUI.kt
+++ src/main/kotlin/com/github/Ringoame196/GUIs/GUI.kt
@@ -2,8 +2,10 @@ package com.github.Ringoame196.GUIs
 
 import org.bukkit.entity.Player
 import org.bukkit.inventory.Inventory
+import org.bukkit.inventory.ItemStack
 
 interface GUI {
+    val guiName:String
     fun createGUI(player: Player): Inventory?
-    fun clickItem()
+    fun clickItem(player: Player,item:ItemStack)
 }
