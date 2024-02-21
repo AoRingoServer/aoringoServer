6b4174a053477e2de16e46ab7036d899d76e1c28
りんご飴196 <kazumac41@gmail.com>
2024-02-21 21:52:04 +0900

    ラムダ式に playerを入力させるように

--- src/main/kotlin/com/github/Ringoame196/Commands/AoringoopCommand.kt
+++ src/main/kotlin/com/github/Ringoame196/Commands/AoringoopCommand.kt
@@ -18,31 +18,33 @@ import org.bukkit.inventory.meta.BookMeta
 import org.bukkit.plugin.Plugin
 
 class AoringoopCommand(val plugin: Plugin) : CommandExecutor, TabExecutor {
-    private var player: Player? = null
     private val subCommand = mapOf(
-        "updateResourcePack" to { ResourcePack(plugin).update() },
-        "tpTestWorld" to { player?.teleport(Bukkit.getWorld("testworld")?.spawnLocation ?: player?.location!!) },
-        "resetShopLand" to { ShopLand().resetShopLand(plugin) },
+        "updateResourcePack" to { player: Player -> ResourcePack(plugin).update() },
+        "tpTestWorld" to { player: Player -> player.teleport(Bukkit.getWorld("testworld")?.spawnLocation ?: player?.location!!) },
+        "resetShopLand" to { player: Player -> ShopLand().resetShopLand(plugin) },
         "fullRecovery" to {
-            player?.health = 20.0
-            player?.foodLevel = 20
-            player?.saturation = 20.0f
-            player?.sendMessage("${ChatColor.AQUA}全回復しました")
+            player: Player ->
+            player.health = 20.0
+            player.foodLevel = 20
+            player.saturation = 20.0f
+            player.sendMessage("${ChatColor.AQUA}全回復しました")
         },
         "loadYML" to {
+            player: Player ->
             Yml().callData(plugin)
-            player?.sendMessage("${ChatColor.YELLOW}[青リンゴサーバー] ymlファイルを再読込しました")
+            player.sendMessage("${ChatColor.YELLOW}[青リンゴサーバー] ymlファイルを再読込しました")
         },
-        "resetHardcore" to { HardcoreWorld().resetHardCoreWorld(plugin) },
-        "bookAuthorChange" to { bookAuthorChange(player) },
+        "resetHardcore" to { player: Player -> HardcoreWorld().resetHardCoreWorld(plugin) },
+        "bookAuthorChange" to { player: Player -> bookAuthorChange(player) },
         "updateResources" to {
+            player: Player ->
             val gui = UpDateResourcesGUI(plugin).createGUI(player)
-            player?.openInventory(gui)
+            player.openInventory(gui)
         }
     )
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
-        player = sender as Player
-        subCommand[args[0]]?.invoke()
+        val player = sender as Player
+        subCommand[args[0]]?.invoke(player)
         return true
     }
 
